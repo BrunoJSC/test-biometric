ac0dd6ccffce8b4f4a056c5b205e8a0e0a6483e5
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-03-08 18:57:47 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+
+import * as LocalAuthentication from "expo-local-authentication";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [biometricType, setBiometricType] = useState<string | null>(null);
+
+  async function checkBiometrics() {
+    const isSupported = await LocalAuthentication.hasHardwareAsync();
+    if (isSupported) {
+      const biometricTypes =
+        await LocalAuthentication.supportedAuthenticationTypesAsync();
+      setBiometricType(biometricTypes[0].toString());
+    } else {
+      console.log("Biometric authentication not supported");
+    }
+  }
+
+  async function authenticate() {
+    const { success } = await LocalAuthentication.authenticateAsync();
+    if (success) {
+      console.log("Authenticated successfully");
+    } else {
+      console.log("Authentication failed");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Check biometrics" onPress={checkBiometrics} />
+      <Button title="Authenticate" onPress={authenticate} />
     </View>
   );
 }
@@ -13,8 +40,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    marginBottom: 20,
   },
 });
